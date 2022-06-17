1aa2aedd5dfe7b43490b4ba9ad7699e45c227cf2
Thomas Fregene <Thomas.Fregene@Suntrustng.com>
2022-06-17 13:47:45 +0100

    Arrays and Array methods

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const s = 'Hello World!';
-const split = 'technology, computers, IT, code';
+//Arrays- variables that hold multiple values
 
-console.log(s.length);
-console.log(s.toUpperCase());
-console.log(s.toLowerCase());
-console.log(s.substring(0,5).toUpperCase());
-console.log(s.split(''));
-console.log(split.split(', '));
+//array constructor
+const numbers = new Array(1,2,3,4,5);
+console.log(numbers);
+
+//mostly used
+const fruits = ['apples', 'oranges', 'pears'];
+fruits[3] = 'grapes';
+//adds element to the end of an array
+fruits.push('mangoes')
+//adds element at the beginning of an array
+fruits.unshift('strawberries');
+//remove the last element from an array
+fruits.pop();
+//checks if variable is an array
+console.log(Array.isArray(fruits));
+//checks the index of an element in an array
+console.log(fruits.indexOf('oranges'));
+
+console.log(fruits);
