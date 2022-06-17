8c1c25546d414fdce77496415f4c5282db2b66f3
Thomas Fregene <Thomas.Fregene@Suntrustng.com>
2022-06-17 15:38:18 +0100

    arrow function

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 //functions
-function addNums(num1 = 1, num2 = 1){
-    //console.log(num1 + num2);
-    return num1 + num2;
-}
+const addNums = (num1, num2)=> num1 + num2;
 
 console.log(addNums(5, 5));
