40a6e6dc092f8797e81e662e8e9258f5483b461f
Thomas Fregene <Thomas.Fregene@Suntrustng.com>
2022-06-17 12:55:26 +0100

    primitive data types

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-//var, let, const
-let score;
-score = 31
+//String, Numbers, Boolean, null, undefined
 
-const age = 40;
-console.log(score);
+const name = 'John';
+const age = 30;
+const ratings = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
 
-console.log(age);
+console.log(typeof z);
