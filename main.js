a09d74d8e737639e31c910ce9323b2e127f3865b
Thomas Fregene <Thomas.Fregene@Suntrustng.com>
2022-06-17 13:05:45 +0100

    string concatenation

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 const name = 'John';
 const age = 30;
-const ratings = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z;
 
-console.log(typeof z);
+//Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+//Template String
+console.log(`My name is ${name} and I am ${age}`);
+
+const hello = `My name is ${name} and I am ${age}`;
+
+console.log(hello);
