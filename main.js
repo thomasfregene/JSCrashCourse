607f26894dc4a1c3a011bbc13da887753ecf10af
Thomas Fregene <Thomas.Fregene@Suntrustng.com>
2022-06-17 15:27:22 +0100

    switch statement

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 //conditionals
-const x = 10;
+const x = 11;
 
 const color = x > 10 ? 'red' : 'blue';
 
-console.log(color);
+switch(color){
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT  red or blue');
+        break;
+}
