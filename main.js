99350a508f168ca83efa5ed49c84bf7c0dafb935
ThomasFregene <fregzythomas@gmail.com>
2022-06-27 16:52:00 +0100

    prototyping function

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ function Person(firstName, lastName, dob){
     this.dob = new Date(dob);
 }
 
+Person.prototype.getBirthYear = function(){
+    return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function(){
+    return `${this.firstName} ${this.lastName}`
+
+}
+
 const person1 = new Person('John', 'Doe', '4-3-1980');
 const person2 = new Person('Mary', 'Smith', '3-6-1970');
 
-console.log(person2.dob);
+console.log(person2);
+
+console.log(person1.getBirthYear());
+console.log(person1.getFullName());
